1b5e8cfe552a54eaec489d028b974272a0ac34fa
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-08-27 07:18:02 -0400

    Added SafeAreaView which knows where the safe areas are on all devices. This helps make sure we're not in the hutch/dynamic island

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { useState } from 'react';
 import GameScreen from './screens/GameScreen';
@@ -26,7 +26,7 @@ const App = () => {
 				resizeMode='cover'
 				style={styles.rootScreen}
 				imageStyle={styles.backgroundImage}>
-				{screen}
+				<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
 			</ImageBackground>
 		</LinearGradient>
 	);
